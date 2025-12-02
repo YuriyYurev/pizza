1c93a064b9e3359a1c3f9d920ac4fd29c6328b90
YuriyYurev <iuriev-yuriy@mail.ru>
2025-12-02 08:44:35 +0700

    feat: assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/shared/Header";
 
 const nunito = Nunito({
   subsets: ['cyrillic'],
@@ -24,6 +25,7 @@ export default function RootLayout({
         className={nunito.className}
       >
         <main className="min-h-screen">
+          <Header />
           {children}
         </main>
       </body>
